e476ce3e9a02ecc1c3038cc4bb9e83656a38050b
Will W <wjwat@onionslice.org>
2022-04-01 13:10:26 -0500

    Display Enum name and not value in JSON responses

--- AnimalShelter.Api/Models/Animal.cs
+++ AnimalShelter.Api/Models/Animal.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace AnimalShelter.Api.Models
 {
   public class Animal
@@ -7,6 +10,7 @@ namespace AnimalShelter.Api.Models
     public string Species { get; set; }
     public string Breed { get; set; }
     public string Description { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public Status Status { get; set; }
   }
 
