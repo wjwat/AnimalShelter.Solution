4c209cfd74a54aebc8f4b8e915019e4140a7be9e
Will W <wjwat@onionslice.org>
2022-04-01 17:56:01 -0500

    Update models with comments for Swagger

--- AnimalShelter.Api/Models/Animal.cs
+++ AnimalShelter.Api/Models/Animal.cs
@@ -6,15 +6,35 @@ namespace AnimalShelter.Api.Models
 {
   public class Animal
   {
+    /// <summary>
+    /// ID of current animal
+    /// </summary>
     public int AnimalId { get; set; }
+
+    /// <summary>
+    /// Name of the animal
+    /// </summary>
     [Required, StringLength(40, MinimumLength = 1)]
     public string Name { get; set; }
+
+    /// <summary>
+    /// Species of the animal
+    /// </summary>
     [Required, StringLength(40, MinimumLength = 1)]
     public string Species { get; set; }
+
+    /// <summary>
+    /// Breed of the animal
+    /// </summary>
     [Required, StringLength(40, MinimumLength = 1)]
     public string Breed { get; set; }
+
+    /// <summary>
+    /// A description of the animal
+    /// </summary>
     [Required]
     public string Description { get; set; }
+
     [Required]
     [EnumDataType(typeof(Status))]
     [JsonConverter(typeof(JsonStringEnumConverter))]
@@ -23,6 +43,9 @@ namespace AnimalShelter.Api.Models
 
   // This would fit better somewhere else instead of just in 
   // AnimalShelter.Api.Models.Status, but I'm not sure where.
+  /// <summary>
+  /// Status of the animal, may be a 0-th index value in the Enum array
+  /// </summary>
   public enum Status
   {
     Available,
